9c55fc968cfc8e2f4762885779271a39aa13b012
orangeburrito <mali.kewl.pool@gmail.com>
2019-02-21 16:53:19 +0700

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,18 +1,34 @@
 main()
 
 function main() {
+    const storageKey = 'markdown'
+
     const textArea = document.getElementById('markdown')
+
+    textArea.value = loadMarkdown()
+
     const wordCount = document.getElementById('word-count')
 
     textArea.addEventListener('input', handleTextInput)
 
     function handleTextInput(event) {
+        // update word count
         let count = event.currentTarget.value.split(/\s+/)
         .filter(nonEmptyString).length
-        wordCount.textContent = count
+        wordCount.textContent = counthr
+        //
+        saveMarkdown(event.currentTarget.value)
     }
 
     function nonEmptyString(text) {
         return text.length > 0
+    }
+
+    function saveMarkdown(str) {
+        localStorage.setItem(storageKey, str)
+    }
+
+    function loadMarkdown() {
+        return localStorage.getItem(storageKey)
     }
 }
